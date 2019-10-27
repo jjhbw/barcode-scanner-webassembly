78fc0c6e1127a7dec78e1e637e8df27c69d997f8
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2019-10-27 13:27:18 +0100

    Library.js now uses 'UTF8ToString' instead of 'Pointer_stringify' to be compatible with the latest version of Emscripten (issue #2)

--- library.js
+++ library.js
@@ -1,21 +1,26 @@
-
 mergeInto(LibraryManager.library, {
     js_output_result: function (symbol, data, polygon, polygon_size) {
-
         // function provided by Emscripten to convert WASM heap string pointers to JS strings.
-        const Pointer_stringify = Module['Pointer_stringify'];
+        const Pointer_stringify = Module["UTF8ToString"];
 
         // Note: new TypedArray(someBuffer) will create a new view onto the same memory chunk, 
         // while new TypedArray(someTypedArray) will copy the data so the original can be freed.
-        const resultView = new Int32Array(Module.HEAP32.buffer, polygon, polygon_size * 2);
+        const resultView = new Int32Array(
+            Module.HEAP32.buffer,
+            polygon,
+            polygon_size * 2
+        );
         const coordinates = new Int32Array(resultView);
 
         // call the downstream processing function that should have been set by the client code
-        const downstreamProcessor = Module['processResult'];
+        const downstreamProcessor = Module["processResult"];
         if (downstreamProcessor == null) {
-            throw new Error("No downstream processing function set")
+            throw new Error("No downstream processing function set");
         }
-        downstreamProcessor(Pointer_stringify(symbol), Pointer_stringify(data), coordinates)
-
+        downstreamProcessor(
+            Pointer_stringify(symbol),
+            Pointer_stringify(data),
+            coordinates
+        );
     }
 });
